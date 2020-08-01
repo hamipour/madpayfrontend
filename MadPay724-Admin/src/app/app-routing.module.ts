58133c6efec50049aefdea9430f6044dd3ae89da
hami <hamipour@gmail.com>
2020-08-01 08:38:10 +0430

    before creating panel and login

--- MadPay724-Admin/src/app/app-routing.module.ts
+++ MadPay724-Admin/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { PanelComponent } from './panel/panel.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: PanelComponent },
+  // { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MadPay724-Admin/src/app/app.module.ts
+++ MadPay724-Admin/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { PanelModule } from './panel/panel.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginModule,
+    PanelModule
   ],
   imports: [
     BrowserModule,
